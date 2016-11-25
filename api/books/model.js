1985dda21c1adfcd01b6ce9c5f56471765d2a139
Robyn Larsen <r@robynlarsen.ca>
2016-11-25 10:29:17 -0500

    Add book schema

--- api/books/model.js
+++ api/books/model.js
@@ -1,13 +1,27 @@
 var mongoose = require('mongoose');
 
+var QuoteSchema = new mongoose.Schema({
+  content: {
+   type: String,
+  },
+  chapter: Number,
+  page: Number
+});
+
+var NoteSchema = new mongoose.Schema({
+  content: String
+});
+
 var ReaderSchema = new mongoose.Schema({
   user: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: [mongoose.Schema.Types.ObjectId],
     ref: 'User'
   },
   currently_reading: Boolean,
   date_added: Date,
-  bookshelves: String
+  bookshelves: String,
+  quotes: [QuoteSchema],
+  notes: [NoteSchema]
 });
 
 var BookSchema = new mongoose.Schema({
