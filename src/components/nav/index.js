a74d8c2f400a68d30cc2b2e51da90c9fcae640b9
Robyn Larsen <r@robynlarsen.ca>
2016-12-07 15:41:10 -0700

    Update the navigation

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
+import logo from '../../logo.svg';
 import './nav.css';
 
 var Navigation = React.createClass({
   render: function() {
     return  <div className="navigation">
       <div className="container">
-        <Link to='/'>insideCover</Link>
-        <Link to='/quote'>New Quote</Link>
-        <Link to='/note'>New Note</Link>
-      </div>
+        <div className="nav-inner">
+          <div className="nav-left">
+            <Link to='/' className="nav-logo">
+              <img src={logo} className="logo" alt="inside cover" />
+            </Link>
+          </div>
 
+          <div className="nav-right">
+            <Link to='/quote'>New Quote</Link>
+            <Link to='/note'>New Note</Link>
+            <Link className="u-right button button-secondary" to='/'>Login</Link>
+          </div>
+        </div>
+      </div>
     </div>
   }
 });
