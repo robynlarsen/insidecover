dd6a833c269f582a0ddc943f1c10a64044486a3e
Robyn Larsen <r@robynlarsen.ca>
2016-12-07 15:43:24 -0700

    Details on deployment

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ var webpack = require('webpack');
 var webpackMiddleware = require('webpack-dev-middleware');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/insidecover');
+// we want to make sure the application is running.
+// mongoose.connect('mongodb://127.0.0.1/insidecover');
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1/insidecover');
+// to connect to the account then use
+// mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds119578.mlab.com:19578/insidecover');
+// mongoose.connect('process.env.MONGODB_URL');
+
 mongoose.Promise = require('bluebird');
 
 var bodyParser = require('body-parser');
@@ -25,4 +31,6 @@ require('./api/users/model');
 // If none of the above matches, serve public/index.html.
 app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 
-app.listen(8080);
+// heroku runs a random port so we can either take PORT or 8080
+// app.listen(8080);
+app.listen(process.env.PORT || 8080);
