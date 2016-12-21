1476b631369e2b3cb6817556a720df1ba2fecd1a
Robyn Larsen <r@robynlarsen.ca>
2016-12-21 10:10:31 -0700

    Add a11y class to signup form

--- src/components/signupForm/index.js
+++ src/components/signupForm/index.js
@@ -7,7 +7,7 @@ class SignupForm extends Component {
         <form action="//robynlarsen.us7.list-manage.com/subscribe/post?u=f7ac839ae9a024c2df2bacc58&id=1e89d1c7e8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="form form-signup validate" target="_blank" noValidate>
           <div id="mc_embed_signup_scroll">
             <div className="mc-field-group">
-              <label htmlFor="mce-EMAIL a11y">Email Address </label>
+              <label htmlFor="mce-EMAIL" className="a11y">Email Address </label>
               <input type="email" name="EMAIL" placeholder="Your email" className="field-input required email" id="mce-EMAIL" />
               <input type="submit" defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
             </div>
